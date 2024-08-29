bb60d7681aee6ad20d610cba6038f35bec59df16
Ku4nYu <ku4nyu@gmail.com>
2024-08-29 13:32:37 +0800

    acwing-608 in java

---
+++ src/acwing_608.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class acwing_608 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        System.out.printf("DIFERENCA = %d", a * b - c * d);
+    }
+}
