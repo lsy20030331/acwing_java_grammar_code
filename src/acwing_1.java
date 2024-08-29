2a675f8ebd9b33d956ae715203a5744be5f85097
Ku4nYu <ku4nyu@gmail.com>
2024-08-29 13:28:14 +0800

    acwing-1 in java

---
+++ src/acwing_1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class acwing_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt(),  y = sc.nextInt();
+        System.out.println(x+y);
+    }
+}
+//练习十遍
