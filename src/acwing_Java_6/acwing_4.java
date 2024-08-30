c101749abadd730d50006e661952dfbe04fa3aec
Ku4nYu <ku4nyu@gmail.com>
2024-08-30 21:57:04 +0800

    v2024.8.30.21.56

--- src/acwing_Java_6/acwing_4.java
+++ src/acwing_Java_6/acwing_4.java
@@ -1,20 +1,21 @@
 package acwing_Java_6;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class acwing_4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-    }
-
-    private static void sort(int[] a, int l, int r){
-        int left = 0;
-        int right = a.length-1;
-        int pleft = left;
-        int pright = right;
+        int l = sc.nextInt();
+        int r = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr, l ,r + 1);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
---
+++ src/acwing_Java_6/acwing_5.java
@@ -0,0 +1,10 @@
+package acwing_Java_6;
+
+import java.util.Scanner;
+
+public class acwing_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
